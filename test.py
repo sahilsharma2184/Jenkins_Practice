01dc257ac5c87290fcc0385fdd1f9870730e62f0
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-10-03 01:00:38 +0530

    initial commit

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 print ("Testing the integration of github with jenkins for testing purpose.")
 print ("The flow will be like Developer's Code -> Git -> Jenkins Server")
+print ("Well the GitHub Integration Job executed very well, now it's the time for GitHub AutoBuild!")
