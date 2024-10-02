b780335131cab5e006eb5627a6ed4880136a55ea
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-10-03 01:15:17 +0530

    initial commit

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ print ("Testing the integration of github with jenkins for testing purpose.")
 print ("The flow will be like Developer's Code -> Git -> Jenkins Server")
 ############################################################################
 print ("Well the GitHub Integration Job executed very well, now it's the time for GitHub AutoBuild!")
-print ("Using the Poll SCM in the build trigger section for this, and adding a cron job to run the script every minute, but the output will be there only incase that there are any changes made.")
+print ("Using the Poll SCM in the build trigger section for this, and adding a cron job to run the script every minute, but the execution will be there only incase that there are any changes made.")
